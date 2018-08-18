52c1440203a7ac9f1ad6c764c883e2f569951714
Yanchevsky Vladislav <vlad13051999@gmail.com>
2018-08-18 19:15:47 +0300

    initial profile

--- GuideHaven/Migrations/20180818150333_somecounts.Designer.cs
+++ GuideHaven/Migrations/20180818150333_somecounts.Designer.cs
@@ -4,14 +4,16 @@ using GuideHaven.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GuideHaven.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20180818150333_somecounts")]
+    partial class somecounts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GuideHaven/Migrations/20180818150333_somecounts.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GuideHaven.Migrations
+{
+    public partial class somecounts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
